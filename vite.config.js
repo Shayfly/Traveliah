f6dd8e484b67c996c03e48a65f3a9dc9a8ee3bcc
Shayfly <shaydoors84@gmail.com>
2025-06-23 04:29:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-export default {
-  base: '/Traveliah/',
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/Traveliah/', // חובה לשים את שם הריפו כאן
+  plugins: [react()],
+})
